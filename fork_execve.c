75d69c76c522141f0679712577991d99a87886e3
Yermehyaw <yermehyaw@gmail.com>
2024-03-01 15:06:26 -0800

    FincEdt: Add buf free option to fork_execve()

--- fork_execve.c
+++ fork_execve.c
@@ -3,13 +3,14 @@
 
 /**
  * fork_execve - Fork and execute a command
- * @cmd_path: command path
+ * @cmd_path: Alloc'ed memory. Path to command
  * @args: Command arguments
+ * @mod: Command modifier, frees @cmd_path when set to 1, otherwise dosent
  *
  * Return: Always 0 (Success)
  */
 
-int fork_execve(char *cmd_path, char **args)
+int fork_execve(char *cmd_path, char **args, int mod)
 {
 	/*pid_t parent_pid;*/
 	pid_t child_pid;
@@ -21,15 +22,19 @@ int fork_execve(char *cmd_path, char **args)
 		return (1);
 	if (child_pid == 0) /* In child process */
 	{
-		if (access(cmd_path, X_OK) == -1) /* if pid can exec program */
-			return (-1);
+		if (access(cmd_path, X_OK) == -1) /*if pid can exec program*/
+			return (1);
 		if (execve(cmd_path, args, NULL) == -1)
+		{
+			perror("Couldnt exec command\n");
 			exit (1);
+		}
 	}
 	if (child_pid > 0)
 	{
 		wait(&exit_code);
-		free(cmd_path);
+		if (mod == 1)
+			free(cmd_path);
 		return (0);
 	}
 	return (1);
