aeebc528bdb0cbff3ec8608e32d2d0951e638102
Yermehyaw <yermehyaw@gmail.com>
2024-03-01 15:21:19 -0800

    FuncEdt: Remove alloc_arr_str() definition

--- program_path.c
+++ program_path.c
@@ -39,48 +39,6 @@ int program_path(char *buf, char **env)
 	return (0);
 }
 
-/**
- * alloc_arr_str - Allocate memory for an array of strings amd initialize
- * tokens from a string delim by white space
- * @buf: String
- * @size: Length of string
- *
- * Return: Memory location of the array (Success)
- */
-
-char **alloc_arr_str(char *buf, int size)
-{
-	int i;
-	char **arr_str, *tmp_str, *tmp_buf;
-
-	arr_str = NULL;
-	tmp_buf = malloc((len_str(buf) + 1) * sizeof(char));
-	if (tmp_buf == NULL)
-		return (NULL);
-	cpy_str(tmp_buf, buf);
-	arr_str = malloc(size);
-	if (arr_str == NULL)
-	{
-		free(tmp_buf);
-		return (NULL);
-	}
-	tmp_str = strtok(tmp_buf, " ");
-	if (tmp_str == NULL) /* No whitespace demim, single command? */
-	{
-		free(tmp_buf);
-		free(arr_str);
-		return (NULL);
-	}
-	for (i = 0; tmp_str; ++i)
-	{
-		arr_str[i] = malloc(len_str(tmp_str) + 1);/*Put sizeof yrself*/
-		cpy_str(arr_str[i], tmp_str);
-		tmp_str = strtok(NULL, " ");
-	}
-	arr_str[i] = NULL;
-	free(tmp_buf);
-	return (arr_str); /* Used for args */
-}
 
 
 /**
@@ -137,7 +95,7 @@ void program_path_exec(char *found_path, char **args)
 	/* Memory is alloc'ed for command_path in cmd_concat */
 	if (command_path == NULL)
 		return;
-	success = fork_execve(command_path, args);
-	if (success != 0) /* fork_exec() not yet successful */
+	success = fork_execve(command_path, args, 1);
+	if (success != 0) /* fork_exec() not successful */
 		free(command_path);
 }
